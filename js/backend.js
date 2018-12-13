c3bb48cabf856981ba1ad4a97b5457ad8aa61c7f
SergeyYakimov <teriertis@yandex.ru>
2018-12-13 23:21:58 +0800

    Made function receiveXhr in backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,58 +1,45 @@
 'use strict';
 
 (function () {
-  var TIMEOUT = 15000;
-  var STATUS = {
-    done: 200
+  var Code = {
+    DONE: 200
+  };
+  var Method = {
+    GET: 'GET',
+    POST: 'POST'
+  };
+  var TEXT_ERROR_STATUS = 'Cтатус ответа: /status/ /statusText/';
+  var TEXT_OF_ERROR = 'Произошла ошибка соединения c сетью';
+  var timeout = 15000;
+  var textOfTimeout = 'Запрос на сервер не успел выполниться за ' + timeout + 'мс';
+
+  var receiveXhr = function (method, url, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === Code.DONE) {
+        onLoad(xhr.response);
+      } else {
+        onError('Не получилось загрузить информацию' + TEXT_ERROR_STATUS.replace('/status/', xhr.status).replace('/statusText/', xhr.statusText));
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError(TEXT_OF_ERROR);
+    });
+    xhr.addEventListener('timeout', function () {
+      onError(textOfTimeout);
+    });
+    xhr.timeout = timeout;
+    xhr.open(method, url);
+    return xhr;
   };
 
   var load = function (url, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === STATUS.done) {
-        onLoad(xhr.response);
-      } else {
-        onError('Ошибка: не получилось загрузить данные с сервера! Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = TIMEOUT;
-
-    xhr.open('GET', url);
-    xhr.send();
+    receiveXhr(Method.GET, url, onLoad, onError).send();
   };
 
   var save = function (url, data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === STATUS.done) {
-        onLoad();
-      } else {
-        onError('Ошибка: не получилось отправить данные на сервер! Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Проблема соединения с сетью');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не был выполнен за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = TIMEOUT;
-
-    xhr.open('POST', url);
-    xhr.send(data);
+    receiveXhr(Method.POST, url, onLoad, onError).send(data);
   };
 
   window.backend = {
